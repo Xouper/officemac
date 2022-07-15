daa68170f395a9e76af909f59b01273b7fdf7816
xouper <xouper01@mail.ru>
2022-07-15 17:02:19 +0300

    test

--- hello-worlds.js
+++ hello-worlds.js
@@ -1,1 +1,16 @@
-console.log('hi')
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+  
+
+function multiplyNumeric(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] === 'number') {
+            obj[key] *= 2
+        }
+    }
+    console.log(obj)
+}
+multiplyNumeric(menu)
