eeeaafa3d4798259325f2d9662cf7fd372a0b551
xouper <xouper01@mail.ru>
2022-07-19 17:30:00 +0300

    recursive pass

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -1,7 +1,8 @@
 describe('My First Test', () => {
     it('Visits the Kitchen Sink', () => {
         cy.visit('https://example.cypress.io/commands/actions')
-           .get('#action-canvas').click()
+           .get('#action-canvas')
+           .click()
            
     })
   })
--- main/hello-worlds.js
+++ main/hello-worlds.js
@@ -1,14 +1,37 @@
-function sumInput() {
-    let numbers = []
-    while (true) {
-        let value = prompt()
-        if (value == '' || !isFinite(value) || value == null) break
-        numbers.push(+value)
-    }
-    let sum = 0
-    for (el of numbers) {
-        sum += el
+let company = {
+  sales: [{
+    name: 'John',
+    salary: 3000,
+  },
+  {
+    name: 'Lisa',
+    salary: 1000,
+  }],
+  development: {
+    sites: [{
+      name: 'Alex',
+      salary: 4500,
+    },
+    {
+      name: "Kate",
+      salary: 400,
+
+  }],
+    internals: [{
+      name: "Dima",
+      salary: 10000,
+    }]
+  }
+}
+function sumSalaries(dev) {
+  let sum = 0
+  if (Array.isArray(dev)) {
+    return dev.reduce((prev, current) => prev + current.salary, sum)
+  } else {
+    for (el of Object.values(dev)){
+      sum += sumSalaries(el)
     }
     return sum
+  }
 }
-alert(sumInput())
+console.log(sumSalaries(company))
